8e681294c179ba8c905ae89794aacacd3c4522e6
know <liujiaweilove7@gmail.com>
2025-12-26 11:14:16 +0800

    删除掉了不必要的js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-// src/router/index.js
+
 import { createRouter, createWebHistory } from "vue-router";
 import Cookie from "js-cookie";
 const routes = [
--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   // 打包配置
   build: {
-    target: "modules", // 设置最终构建的浏览器兼容目标。modules:支持原生 ES 模块的浏览器
+    // target: "modules", // 设置最终构建的浏览器兼容目标。modules:支持原生 ES 模块的浏览器
     outDir: "dist", // 指定输出路径
     assetsDir: "assets", // 指定生成静态资源的存放路径
     sourcemap: false, // 构建后是否生成 source map 文件
